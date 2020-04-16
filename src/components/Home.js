079fed37ca129f5190f572d3d686e240ccfb7538
unknown <husainartist5253@gmail.com>
2020-04-16 09:44:54 -0400

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,6 @@ class HomePage extends Component {
   render() {
     return (
       <div >
-        <h1>Welcome</h1>
         <img style={ sectionStyle } src={ Background } />
       </div>
         
